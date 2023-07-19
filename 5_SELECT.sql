cb2b324d7fc32b7eebf1f4ff81f004141e32a8a1
Mykyta Timoshenko <116592259+NickTimosh@users.noreply.github.com>
2023-07-20 00:26:52 +0300

    Update 5_SELECT.sql

--- 5_SELECT.sql
+++ 5_SELECT.sql
@@ -2,14 +2,14 @@
   
 SELECT
     	1	  AS nmb_1
-	    ,2	  AS nmb_2
-	    ,5+5  AS nmb_3;
+	,2	  AS nmb_2
+	,5+5      AS nmb_3;
 
 # повернення кількості значень в таблиці і колонці, а також унікальних значень в колонці
   
 SELECT 
 	COUNT(*)			        AS cnt_all
-	, COUNT(exp_level)	  AS cnt_level
+	, COUNT(exp_level)	 		 AS cnt_level
 	, COUNT(DISTINCT exp_level)
 FROM salaries
 LIMIT 10;
@@ -17,9 +17,9 @@ LIMIT 10;
 # повернення деяких описових статистик по вказаній колонці
   
 SELECT 
-	AVG(salary_in_usd)	   AS salary_avg
-	, MIN(salary_in_usd)   AS salary_min
-	, MAX(salary_in_usd)   AS salary_max
+	AVG(salary_in_usd)	   	AS salary_avg
+	, MIN(salary_in_usd)   		AS salary_min
+	, MAX(salary_in_usd)   		AS salary_max
 FROM salaries
 WHERE year = '2023'
 LIMIT 10;
