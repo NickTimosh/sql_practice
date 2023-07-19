a81e2065060bd3111f73e286c7e2c0ab3956279e
Mykyta Timoshenko <116592259+NickTimosh@users.noreply.github.com>
2023-07-20 00:20:06 +0300

    Create 5_SELECT.sql

---
+++ 5_SELECT.sql
@@ -0,0 +1,42 @@
+# повернення значень без використання будь-якого джерела
+  
+SELECT
+    	1	    AS nmb_1
+	    ,2	  AS nmb_2
+	    ,5+5	AS nmb_3;
+
+# повернення кількості значень в таблиці і колонці, а також унікальних значень в колонці
+  
+SELECT 
+	COUNT(*)			        AS cnt_all
+	, COUNT(exp_level)	  AS cnt_level
+	, COUNT(DISTINCT exp_level)
+FROM salaries
+LIMIT 10;
+
+# повернення деяких описових статистик по вказаній колонці
+  
+SELECT 
+	AVG(salary_in_usd)	   AS salary_avg
+	, MIN(salary_in_usd)   AS salary_min
+	, MAX(salary_in_usd)   AS salary_max
+FROM salaries
+WHERE year = '2023'
+LIMIT 10;
+
+# впровадження нових колонок за розрахунком та умовою
+
+SELECT 
+	year
+	, exp_level
+	, salary_in_usd
+	, salary_in_usd * 38	      AS salary_in_uah
+	, CASE
+		WHEN exp_level = 'SE'
+		  THEN 'Senior'
+		WHEN exp_level = 'MI'
+		  THEN 'Middle'
+		ELSE 'Other' 
+    END 		                  AS full_level
+FROM salaries
+LIMIT 100;
