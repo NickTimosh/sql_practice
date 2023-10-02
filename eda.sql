60f5bf2bbf88dc6c1c46ecbddbd8416ac38d7f5b
Nikita Tymoshenko <116592259+NickTimosh@users.noreply.github.com>
2023-10-02 15:20:07 -0400

    Update eda.sql

--- eda.sql
+++ eda.sql
@@ -1,12 +1,12 @@
--- Select a few rows to check (no order) -------------
+-- Select a few rows to check (no order) ---------------------------------------
 SELECT count(*) FROM salaries;
 
---  Count missing values -------------
+--  Count missing values ----------------------------------------------------
 SELECT 
     COUNT(*) - COUNT(salary_in_usd) AS missing
 FROM salaries;
 
--- A few words about data types -------------
+-- A few words about data types ----------------------------------------------------
 
 -- Summarizing and aggregating numeric data
 -- Range: Min and Max
@@ -23,6 +23,22 @@ SELECT
 FROM salaries
 GROUP BY 1;
 
+-- Exploring distributions for finding errors, outliers...
 
+SELECT 
+    remote_rate
+    , COUNT(*)
+FROM salaries -- for small number of discrete values
 
+-- Generate series to create bins
+...
+    
+-- More summary functions:
+SELECT CORR(revenues,profits) AS rev_profits,
+	   -- Correlation between revenues and assets
+       CORR(revenues, assets) AS rev_assets,
+       -- Correlation between revenues and equity
+       CORR(revenues, equity) AS rev_equity 
+  FROM fortune500;
 
+-- Exploring categorical data and instractured text ----------------------------------------------------
