dba8a0af0b4f57339fba5be8ce19b64c46c522fc
Nikita Tymoshenko <116592259+NickTimosh@users.noreply.github.com>
2023-11-27 22:33:21 -0500

    Update 10_inner_join.sql

--- 10_inner_join.sql
+++ 10_inner_join.sql
@@ -1,18 +1,28 @@
-SELECT * FROM Invoice i LIMIT 100;
-SELECT * FROM InvoiceLine il LIMIT 100;
-SELECT * FROM Track t LIMIT 100;
-SELECT * FROM Album a LIMIT 100;
-SELECT * FROM Artist a LIMIT 100;	
-
--- Inner Join
-SELECT 
-	t.TrackId 
-	, t.Name 
-	, a.Title 		AS album_title
-	, a2.Name 
-FROM Track t 
-INNER JOIN Album a 	ON t.AlbumId = a.AlbumId  
-INNER JOIN Artist a2 	ON a.ArtistId = a2.ArtistId 
-WHERE a2.Name LIKE 'A%'
-LIMIT 100
-;
+SELECT * FROM Invoice i LIMIT 100; 
+SELECT * FROM InvoiceLine il LIMIT 100; 
+SELECT * FROM Track t LIMIT 100; 
+SELECT * FROM Album a LIMIT 100; 
+SELECT * FROM Artist a LIMIT 100; 
+ 
+SELECT  
+ t.TrackId  
+ , t.Name  
+ , a.Title  
+ , a.ArtistId  
+ , art.Name  
+FROM Track t  
+JOIN Album a  	ON t.AlbumId = a.AlbumId  
+JOIN Artist art ON a.ArtistId = art.ArtistId  
+WHERE art.Name LIKE "A%" 
+LIMIT 100; 
+ 
+SELECT  
+ art.Name 
+ , COUNT(t.TrackId) 
+FROM Track t  
+JOIN Album a  	ON t.AlbumId = a.AlbumId  
+JOIN Artist art ON a.ArtistId = art.ArtistId  
+WHERE art.Name LIKE "A%" 
+GROUP BY 1 
+ORDER BY 2 DESC 
+LIMIT 100;
